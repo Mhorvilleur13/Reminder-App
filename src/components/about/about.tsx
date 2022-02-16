b05ce2540ed64c61c2c9d519249d21e7dbb85c98
Felix Razykov <frazykov@gmail.com>
2022-02-16 20:20:04 +0200

    Add About page

---
+++ src/components/about/about.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = ({}: {}) => {
+  return (
+    <div>
+      <h2>About the app</h2>
+      <p>The app is a simple reminder app</p>
+    </div>
+  );
+};
+
+export default About;
