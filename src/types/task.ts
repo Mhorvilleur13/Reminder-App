80127cb78a53aa3ab6411b1891714657a01e041a
Felix Razykov <frazykov@gmail.com>
2022-02-16 19:42:29 +0200

    Add base types for task, tag, reminder, recurrence etc.

--- src/types/task.ts
+++ src/types/task.ts
@@ -1,4 +1,11 @@
-export interface ITask {
+import { RecurrenceConfig } from "./recurrence-config";
+import { ReminderConfig } from "./reminder-config";
+import { Tag } from "./tag";
+
+export interface Task {
   taskName: string;
+  tags: Tag[];
   recurring: boolean;
+  recurrenceConfig: RecurrenceConfig;
+  reminderConfig: ReminderConfig;
 }
