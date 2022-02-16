48c25d6f8cf2aaf3354ba1e9104a68f689ffb65b
Felix Razykov <frazykov@gmail.com>
2022-02-16 20:21:44 +0200

    Use router to navigate between pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,31 @@
 import React from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import About from "./components/about/about";
+import Form from "./components/reminder-form/reminder-form";
 
 const App = ({}: {}) => {
   return (
     <div className="mt-4 container">
-      <h1>Reminder app</h1>
+      <div className="row">
+        <div className="col">
+          <Link to="/" className="text-decoration-none">
+            <h1>Reminder app</h1>
+          </Link>
+        </div>
+        <div className="col">
+          <div className="text-right">
+            <Link to="/about" className="btn btn-primary">
+              About
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="mt-4">
+        <Routes>
+          <Route path="/" element={<Form />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
     </div>
   );
 };
