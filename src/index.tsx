ef8ea2f835647642897ce784d07f0de55aed8f0f
Felix Razykov <frazykov@gmail.com>
2022-02-16 19:49:06 +0200

    Add recoil

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { RecoilRoot } from "recoil";
 import "bootstrap/dist/css/bootstrap.css";
 
 import App from "./App";
@@ -7,7 +8,9 @@ import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
 );
