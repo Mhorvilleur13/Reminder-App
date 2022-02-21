881548d0219fab947817dbe65d48d5883274280f
Michael Horvilleur <mhorvilleur13@gmail.com>
2022-02-21 12:54:14 -0600

    HTML for the form

--- src/components/reminder-form/reminder-form.tsx
+++ src/components/reminder-form/reminder-form.tsx
@@ -1,18 +1,46 @@
-import React, { FormEvent } from "react";
+import React, { FormEvent, useState } from "react";
 
-const Form = ({}: {}) => {
+
+const Form = ({ }: {}) => {
+  const [taskName, setTaskName] = useState('');
+  const [recurring, setRecurring] = useState(false);
   const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     e.preventDefault();
   };
   return (
     <form onSubmit={handleSubmit} className="form-group">
       <div>
-        <input type="text" className="form-control" placeholder="Add a remider" />
+        <input type="text" className="form-control" placeholder="Remider Title" />
       </div>
-      <div className="mt-2">
-        <input type="text" className="form-control" placeholder="How do you want to be reminded?" />
+      <div className="card mt-3 text-secondary ">
+        <h6>What would you like the reminder to say?</h6>
+        <textarea id="custom-message" ></textarea>
       </div>
-      <div className="mt-2">
+      <div className="mt-3">
+        <input type="text" className="form-control" placeholder="How would you like to be reminded?" />
+      </div>
+      <div className=" card mt-3 text-secondary">
+        <h6>When would you like to be reminded?</h6>
+        <input type="date"></input>
+      </div>
+      <div className="card mt-3 text-secondary" >
+        <h6>Is this a recurring task?</h6>
+        <button className="btn btn-primary col-2" type="button"> YES</button>
+      </div>
+      <div className="card mt-3 text-secondary">
+        <h6>How often would would like to be reminded?</h6>
+        <select id="reminder-frequency" name="reminder">
+          <option value="hourly">Hourly</option>
+          <option value="daily">Daily</option>
+          <option value="weekly">Weekly</option>
+          <option value="anually">Anually</option>
+        </select>
+      </div>
+      <div className="card mt-3 text-secondary">
+        <h6>How many hours before would you like to be reminded?</h6>
+        <input type="number" className="col-1"></input>
+      </div>
+      <div className="mt-3">
         <button type="submit" className="btn btn-primary">
           Add reminder to your list
         </button>
