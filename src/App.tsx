a2b83f7f00df2771385ef6825408cadfc0c1f660
Michael Horvilleur <mhorvilleur13@gmail.com>
2022-03-20 13:47:50 -0600

    Added Upcoming Task Component and Page

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,34 @@ import { Task } from "./types/task";
 import AllTasks from "./components/all-tasks/all-tasks";
 import TaskComponent from "./components/task/task";
 import { atom, useRecoilState } from "recoil";
+import UpcomingReminders from "./components/upcoming-reminders/upcoming-reminders";
 
 const App = () => {
   return (
     <div className="mt-4 container">
       <div className="row">
-        <div className="col-4">
+        <div className="col-3">
           <div className="text-center">
             <Link to="/tasks" className="btn btn-primary">
               Reminders
             </Link>
           </div>
         </div>
-        <div className="col-4">
+        <div className="col-3">
           <div className="text-center">
             <Link to="/" className="btn btn-primary">
               Add Task
             </Link>
           </div>
         </div>
-        <div className="col-4">
+        <div className="col-3">
+          <div className="text-center">
+            <Link to="/upcoming" className="btn btn-primary">
+              Upcoming Tasks
+            </Link>
+          </div>
+        </div>
+        <div className="col-3">
           <div className="text-center">
             <Link to="/about" className="btn btn-primary">
               About
@@ -37,6 +45,7 @@ const App = () => {
       <div className="mt-4">
         <Routes>
           <Route path="/tasks" element={<AllTasks />} />
+          <Route path="/upcoming" element={<UpcomingReminders />} />
           <Route path="/" element={<Form />} />
           <Route path="/about" element={<About />} />
         </Routes>
--- src/components/reminder-form/reminder-form.tsx
+++ src/components/reminder-form/reminder-form.tsx
@@ -6,7 +6,8 @@ import { RecurrenceType } from "../../types/recurrence-config";
 import { Task } from "../../types/task";
 import { logger } from "../../services/logger";
 import { atom, useRecoilState } from "recoil";
-import { tasksAtom } from "../../state/atoms";
+import { tasksAtom, upcomingAtom } from "../../state/atoms";
+import moment from "moment";
 
 const Form = () => {
   const [taskName, setTaskName] = useState("");
@@ -17,6 +18,7 @@ const Form = () => {
   const [reccurenceType, setReccuringFrequency] = useState<RecurrenceType>(RecurrenceType.HOURLY);
   const [reminderBeforeMs, setReminderBeforeMs] = useState(1 * HOURS);
   const [tasks, setTasks] = useRecoilState(tasksAtom);
+  const [upcoming, setUpcoming] = useRecoilState(upcomingAtom);
 
   const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
     const removeDash = (str: string) => {
@@ -39,12 +41,22 @@ const Form = () => {
       },
     };
     logger.info("Task form submitted", newTask);
+    //sorts the tasks in order of date
     const newTasks = [...tasks];
     newTasks.push(newTask);
     const sortedTasks = newTasks.sort(
       (a, b) => parseInt(removeDash(a.reminderDate)) - parseInt(removeDash(b.reminderDate))
     );
     setTasks(sortedTasks);
+    //Check if the task is within the next 7 days
+    const now = moment();
+    const input = moment(newTask.reminderDate);
+    if (input.diff(now, "days") <= 7) {
+      const newUpcoming = [...upcoming];
+      newUpcoming.push(newTask);
+      setUpcoming(newUpcoming);
+      console.log(upcoming);
+    }
   };
 
   const recurringTypes = [
