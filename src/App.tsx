22b2628c4794522b1f1c65c243d33435bcf41ffe
Michael Horvilleur <mhorvilleur13@gmail.com>
2022-04-05 23:03:43 -0500

    fixed last commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,14 +16,6 @@ const App = () => {
   const tasks = useRecoilValue(tasksAtom);
   const todayReminders = useRecoilValue(todayTaskState);
   const upcoming = useRecoilValue(upcomingTasksState);
-  useEffect(() => {
-    chrome.storage.sync.set({ key: "value" }, function () {
-      console.log("Value is set to");
-    });
-    chrome.storage.sync.get(["key"], function (result) {
-      console.log("Value currently is " + result.key);
-    });
-  }, []);
   return (
     <div className="mt-4 container page-container">
       <div className="row">
