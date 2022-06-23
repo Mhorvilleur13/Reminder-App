67943887595882f6c67f5735841bc104688555f6
Mhorvilleur13 <mhorvilleur13@gmail.com>
2022-06-23 19:51:53 -0400

    about-section

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -4,7 +4,10 @@ const About = () => {
   return (
     <div>
       <h2>About the app</h2>
-      <p>The app is a simple reminder app</p>
+      <p>
+        We believe the simpler, the better. So we designed a simple, easy to use reminder app that will always be a
+        click away.{" "}
+      </p>
     </div>
   );
 };
